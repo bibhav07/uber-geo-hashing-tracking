d5fdb15527061aeda3fddf06994a3499e30ebc24
bibhav07 <managementlucky07@gmail.com>
2026-01-17 19:31:48 +0530

    feat: added workflows

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,8 +1,10 @@
 // src/services/socket.js
 import { io } from 'socket.io-client';
 
+// If you are on localhost, this becomes http://localhost:4000
+// If you are on AWS 54.123.45.67, this becomes http://54.123.45.67:4000
 // We point to port 4000 (where our backend will run)
-const URL = 'http://localhost:4000';
+const URL =  `${window.location.protocol}//${window.location.hostname}:4000`;
 
 export const socket = io(URL, {
   autoConnect: false // We connect manually in App.jsx
